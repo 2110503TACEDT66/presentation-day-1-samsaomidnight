e03e9f7762a08a380361b448eaa07602d5a741cd
esthxrs <therjintawong@gmail.com>
2024-03-04 12:19:55 +0700

    debug

--- routes/massages.js
+++ routes/massages.js
@@ -3,8 +3,6 @@ const express = require('express');
 const {getMassages, getMassage, createMassage, updateMassage, deleteMassage} = require('../controllers/massages'); 
 
 
-const { getReviewsForMassage } = require('../controllers/review');
-const reviewRouter = require('./review');
 
 //include other resource routers
 const appointmentRouter = require('./appointments');
@@ -13,8 +11,6 @@ const router = express.Router();
 
 const {protect, authorize} = require('../middleware/auth');
 
-router.use('/:massageId/appointments/', appointmentRouter);
-router.use('/:massageId/reviews', reviewRouter);
 
 router.route('/').get(getMassages).post(protect, authorize('admin'), createMassage); 
 router.route('/:id').get(getMassage).put(protect, authorize('admin'), updateMassage).delete(protect, authorize('admin'), deleteMassage); 
