9faa695317b4b306708ac3e7487266631761864c
Penpitcha Ketpetch <prim.penpitcha2004@gmail.com>
2024-03-04 11:30:54 +0700

    Wannabe Extra Review ver4.5 Add ver2

--- routes/massages.js
+++ routes/massages.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const {getMassages, getMassage, createMassage, updateMassage, deleteMassage} = require('../controllers/massages'); 
 const { getReviewsForMassage } = require('../controllers/review');
-
+const reviewRouter = require('./review');
 
 //include other resource routers
 const appointmentRouter = require('./appointments');
@@ -12,6 +12,7 @@ const router = express.Router();
 const {protect, authorize} = require('../middleware/auth');
 
 router.use('/:massageId/appointments/', appointmentRouter);
+router.use('/:massageId/reviews', reviewRouter);
 
 router.route('/').get(getMassages).post(protect, authorize('admin'), createMassage); 
 router.route('/:id').get(getMassage).put(protect, authorize('admin'), updateMassage).delete(protect, authorize('admin'), deleteMassage); 
