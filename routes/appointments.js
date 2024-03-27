ffeb41d8513ca266e0523bbef23f8cf2452a9843
Penpitcha Ketpetch <prim.penpitcha2004@gmail.com>
2024-03-27 12:46:40 +0700

    resolve cf

--- routes/appointments.js
+++ routes/appointments.js
@@ -2,16 +2,9 @@ const express = require('express');
 const {getAppointments, getAppointment, addAppointment, updateAppointment, deleteAppointment} = require('../controllers/appointments');
 const router = express.Router({mergeParams : true});
 
-<<<<<<< HEAD
-const {authorize} = require('../middleware/auth');
-
-router.route('/').get(getAppointments).post(addAppointment);
-router.route('/:id').get(getAppointment).put(updateAppointment).delete(authorize('admin','user'), deleteAppointment);
-=======
 const {authorize, protect} = require('../middleware/auth');
 
 router.route('/').get(protect, getAppointments).post(addAppointment);
 router.route('/:id').get(getAppointment).put(protect, updateAppointment).delete(deleteAppointment);
->>>>>>> 36e89e505d2de23ffb4f08bb09d4d2e8f1b7957b
 
 module.exports = router;
